20203aaccce4d4085c6dacffb657f22e896e7034
yangbh <mody@modydeMacBook-Pro.local>
2014-06-17 21:45:52 +0800

    1

---
+++ lib/mysql.class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2.7
+#coding:utf-8
+'''
+
+'''
+class ClassName(object):
+	"""docstring for ClassName"""
+	def __init__(self, arg):
+		super(ClassName, self).__init__()
+		self.arg = arg
+		
